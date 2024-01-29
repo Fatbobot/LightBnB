b927260f56f4412071e35a081f75b618fbbdf487
Fatbobot <Fatbobot@gmail.com>
2024-01-29 08:26:49 -0400

    fixed search query issues to dynamically work together by adding ternaries

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -101,7 +101,6 @@ const getAllReservations = function (guest_id, limit = 10) {
     });
 };
 /// Properties
-
 /**
  * Get all properties.
  * @param {{}} options An object containing query options.
@@ -113,32 +112,31 @@ const getAllProperties = (options, limit = 10) => {
   let queryString = `
     SELECT properties.*, AVG(property_reviews.rating) AS average_rating
     FROM properties
-    LEFT JOIN property_reviews ON properties.id = property_id`;
+    JOIN property_reviews ON properties.id = property_id`;
 
     if (options.minimum_rating) {
       queryParams.push(`${options.minimum_rating}`);
-      queryString += `AND property_reviews.rating >= $${queryParams.length} `;
+      queryString += ` WHERE property_reviews.rating >= $${queryParams.length} `;
     }
     if (options.owner_id) {
       queryParams.push(`${options.owner_id}`);
-      queryString += `AND owner_id = $${queryParams.length}`;
+      queryString += `${queryParams.length > 1 ? ' AND' : ' WHERE'} owner_id = $${queryParams.length}`;
     }
 
     if (options.city) {
       queryParams.push(`%${options.city}%`);
-      queryString += `AND city LIKE $${queryParams.length} `;
+      queryString += `${queryParams.length > 1 ? ' AND' : ' WHERE'} city LIKE $${queryParams.length} `;
     }
   
     if (options.minimum_price_per_night) {
       queryParams.push(`${options.minimum_price_per_night * 100}`);
-      queryString += `AND cost_per_night >= $${queryParams.length} `;
+      queryString += `${queryParams.length > 1 ? ' AND' : ' WHERE'} cost_per_night >= $${queryParams.length} `;
     }
   
     if (options.maximum_price_per_night) {
       queryParams.push(`${options.maximum_price_per_night * 100}`);
-      queryString += `AND cost_per_night <= $${queryParams.length}`;
+      queryString += `${queryParams.length > 1 ? ' AND' : ' WHERE'} cost_per_night <= $${queryParams.length}`;
     }
-  
   queryParams.push(limit);
   queryString += `
     GROUP BY properties.id
@@ -158,6 +156,51 @@ const getAllProperties = (options, limit = 10) => {
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
+const addProperty = function (property) {
+  const queryParams = [
+    property.owner_id,
+    property.title,
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    property.cost_per_night,
+    property.street,
+    property.city,
+    property.province,
+    property.post_code,
+    property.country,
+    property.parking_spaces,
+    property.number_of_bathrooms,
+    property.number_of_bedrooms
+  ];
+  const queryString = `
+    INSERT INTO properties (
+      owner_id,
+      title,
+      description,
+      thumbnail_photo_url,
+      cover_photo_url,
+      cost_per_night,
+      street,
+      city,
+      province,
+      post_code,
+      country,
+      parking_spaces,
+      number_of_bathrooms,
+      number_of_bedrooms
+    ) VALUES (
+      $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14
+    )
+    RETURNING *;
+  `;
+
+  return pool.query(queryString, queryParams)
+    .then((result) => result.rows[0])
+    .catch((err) => {
+      console.error(err.message);
+    });
+};
 module.exports = {
   getUserWithEmail,
   getUserWithId,
